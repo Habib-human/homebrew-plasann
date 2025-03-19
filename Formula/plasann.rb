826c4485a9cb1729bd4dfb06c119252de5d5f6aa
Habib-human <habibulislam.mist@gmail.com>
2025-03-19 14:14:49 -0400

    Add explicit installation of Python dependencies

--- Formula/plasann.rb
+++ Formula/plasann.rb
@@ -10,24 +10,42 @@ class Plasann < Formula
   depends_on "prodigal"
   depends_on "python@3.9"
 
+  # Define Python resources
+  resource "gdown" do
+    url "https://pypi.io/packages/source/g/gdown/gdown-4.7.1.tar.gz"
+    sha256 "d6d231ceb8a1b375369bb4031c5b0a0380bd0147e9b9aac2d9c3aeda15db6e11"
+  end
+  
+  resource "biopython" do
+    url "https://pypi.io/packages/source/b/biopython/biopython-1.83.tar.gz"
+    sha256 "fc5b28d8d3d7e52fb41914da380b93a1f2cfd3ebcd799abc37e73d94bbe25de2"
+  end
+  
+  resource "pandas" do
+    url "https://pypi.io/packages/source/p/pandas/pandas-2.1.4.tar.gz"
+    sha256 "fcb68203651ecb9d671314c1a618d7ae4470fd961a150e5af3149951703aa742"
+  end
+  
+  resource "matplotlib" do
+    url "https://pypi.io/packages/source/m/matplotlib/matplotlib-3.8.2.tar.gz"
+    sha256 "01a978b871b881ee76017152f1f1a0e0b33d4cadf58d7f9168bebd8d378f4bff"
+  end
+  
+  resource "pycirclize" do
+    url "https://pypi.io/packages/source/p/pycirclize/pycirclize-0.3.1.tar.gz"
+    sha256 "23609bbb4fcd7ef0e9dd95e8e0c1dca2faa1e53bd5e4f9a3e7377ae33ba7a4da"
+  end
+
   def install
-    venv = virtualenv_create(libexec, "python3.9")
+    # Create and activate virtualenv
+    virtualenv_install_with_resources
     
-    # Install dependencies first
-    venv.pip_install "biopython"
-    venv.pip_install "pandas"
-    venv.pip_install "matplotlib"
-    venv.pip_install "pycirclize"
-    venv.pip_install "gdown"
-    
-    # Then install the package
-    venv.pip_install "plasann"
-    
-    # Create a wrapper script
+    # Create a wrapper script that modifies the Python path to find modules
     (bin/"PlasAnn").write <<~EOS
       #!/bin/bash
       export PATH="#{Formula["blast"].opt_bin}:#{Formula["prodigal"].opt_bin}:$PATH"
-      "#{libexec}/bin/PlasAnn" "$@"
+      export PYTHONPATH="#{libexec}/lib/python3.9/site-packages:$PYTHONPATH"
+      "#{libexec}/bin/python3" -m Scripts.annotate_plasmid "$@"
     EOS
     
     chmod 0755, bin/"PlasAnn"
